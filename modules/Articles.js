93a0ef98b84ca167e49c2b3ed954e33df5ba9dba
kanwaljeet5124 <kanwaljeetsingh5124@gmail.com>
2023-03-05 22:54:36 +0530

    Kanwal: Blog page added

---
+++ modules/Articles.js
@@ -0,0 +1,13 @@
+import Article from "./Article"
+export default function Articles() {
+  return (
+    <div className="container article_grid" >
+        <Article/>
+        <Article/>
+        <Article/>
+        <Article/>
+        <Article/>
+        <Article/>
+    </div>
+  )
+}
--- pages/blogs.js
+++ pages/blogs.js
@@ -1,15 +1,32 @@
-import FeaturedBlog from "../modules/FeaturedBlog"
 import Header from "../modules/Header"
+import FeaturedBlogSlider from "../modules/FeaturedBlogSlider"
+import SectionHeading from "../modules/SectionHeading"
+import Articles from "../modules/Articles"
+import LetsTalkBlack from "../modules/LetsTalkBlack"
+import Footer from "../modules/Footer"
 export default function Blogs() {
   return (
     <>
         <Header/>
         <section className="blog-banner">
             <div className="d-flex d-flex-wrap d-justify-center container">
-                <h6 className="col-12 text-primary l-27 m-0 mb-2 f-500 text-align-center">What We Do</h6>
+                <h6 className="col-12 text-primary l-27 m-0 f-500 text-align-center">What We Do</h6>
                 <h2 className="col-12 text-align-center f-400">News & Articles</h2>
+                <span className="col-12 text-align-center text-grey f-400">#1 Blog on theme marketing by Bodrum</span>
+                <form className="blog_form">
+                  <input type="text" placeholder="Search by title, etc…" ></input>
+                  <button type="submit"><img src="/magnifier.svg" alt="magnifier-icon" /></button>
+                </form>
             </div>
-            <FeaturedBlog></FeaturedBlog>
+            <div className="mt-5 pt-5">
+              <FeaturedBlogSlider />
+            </div>
+        </section>
+        <section>
+          <SectionHeading classes="text-align-center" title="Recent  " highlight="Articles" />
+          <Articles />
+          <LetsTalkBlack />
+          <Footer />
         </section>
     </>
   )
